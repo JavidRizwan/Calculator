7067f157de308b2265898cd3014fbe236e6f77f0
Javid Rizwan <javidrizwan1@gmail.com>
2022-02-04 07:00:26 +0530

    initial commit

---
+++ src/features/calculator/calculator.js
@@ -0,0 +1,156 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Element, Clear, Equal } from "./calculatorSlice";
+
+export function Calculator() {
+  const array = useSelector((state) => state.calculator.array);
+  const result = useSelector((state) => state.calculator.result);
+  let display = array.join("");
+  const dispatch = useDispatch();
+
+  return (
+    <div id="calc" className="container-sm">
+      <div className="row display-box">
+        <h5 className="display">{result}</h5>
+        <h4 className="display" id="display">
+          {array.length !== 0 ? display : result}
+        </h4>
+      </div>
+      <div className="row">
+        <div className="col col-9">
+          <div className="row">
+            <button
+              className="btn-danger col-8"
+              id="clear"
+              onClick={() => dispatch(Clear())}
+            >
+              AC
+            </button>
+            <button
+              className="btn col-4"
+              id="divide"
+              onClick={() => dispatch(Element("/"))}
+            >
+              /
+            </button>
+          </div>
+          <div className="row">
+            <button
+              className="btn col-4"
+              id="seven"
+              onClick={() => dispatch(Element("7"))}
+            >
+              7
+            </button>
+            <button
+              className="btn col-4"
+              id="eight"
+              onClick={() => dispatch(Element("8"))}
+            >
+              8
+            </button>
+            <button
+              className="btn col-4"
+              id="nine"
+              onClick={() => dispatch(Element("9"))}
+            >
+              9
+            </button>
+          </div>
+          <div className="row">
+            <button
+              className="btn col-4"
+              id="four"
+              onClick={() => dispatch(Element("4"))}
+            >
+              4
+            </button>
+            <button
+              className="btn col-4"
+              id="five"
+              onClick={() => dispatch(Element("5"))}
+            >
+              5
+            </button>
+            <button
+              className="btn col-4"
+              id="six"
+              onClick={() => dispatch(Element("6"))}
+            >
+              6
+            </button>
+          </div>
+          <div className="row">
+            <button
+              className="btn col-4"
+              id="one"
+              onClick={() => dispatch(Element("1"))}
+            >
+              1
+            </button>
+            <button
+              className="btn col-4"
+              id="two"
+              onClick={() => dispatch(Element("2"))}
+            >
+              2
+            </button>
+            <button
+              className="btn col-4"
+              id="three"
+              onClick={() => dispatch(Element("3"))}
+            >
+              3
+            </button>
+          </div>
+          <div className="row">
+            <button
+              className="btn col-8"
+              id="zero"
+              onClick={() => dispatch(Element("0"))}
+            >
+              0
+            </button>
+            <button
+              className="btn col-4"
+              id="decimal"
+              onClick={() => dispatch(Element("."))}
+            >
+              .
+            </button>
+          </div>
+        </div>
+        <div className="col col-3 p-0 m-0">
+          <button
+            className="btn w-100"
+            id="multiply"
+            onClick={() => dispatch(Element("*"))}
+          >
+            *
+          </button>
+          <button
+            className="btn w-100"
+            id="subtract"
+            onClick={() => dispatch(Element("-"))}
+          >
+            -
+          </button>
+          <button
+            className="btn w-100"
+            id="add"
+            onClick={() => dispatch(Element("+"))}
+          >
+            +
+          </button>
+          <button
+            className="btn w-100"
+            id="equals"
+            onClick={() => dispatch(Equal())}
+          >
+            =
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/features/calculator/calculatorSlice.js
@@ -0,0 +1,116 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  array: [],
+  result: 0,
+};
+
+export const calculatorSlice = createSlice({
+  name: "calculator",
+  initialState,
+  reducers: {
+    Element: (state, action) => {
+      var element = action.payload;
+      var lastElement = state.array[state.array.length - 1];
+      var lastBefore = state.array[state.array.length - 2];
+      switch (element) {
+        case "+":
+        case "/":
+        case "*":
+          //adds result in the front if the first element is an operator
+          if (state.array.length === 0) {
+            state.array.push(state.result.toString());
+          }
+          // Checks if it is the second operator
+          if (
+            (lastElement === "+") |
+            (lastElement === "-") |
+            (lastElement === "/") |
+            (lastElement === "*")
+          ) {
+            state.array.pop();
+            if (
+              (lastBefore === "+") |
+              (lastBefore === "-") |
+              (lastBefore === "/") |
+              (lastBefore === "*")
+            ) {
+              state.array.pop();
+            }
+          }
+          state.array.push(element);
+          break;
+        case "-":
+          //adds result in the front if the first element is an operator
+          if (state.array.length === 0) {
+            state.array.push(state.result.toString());
+          }
+          // removes the last before element and changes last minus from negative number to operator
+          if (state.array[state.array.length - 1] === "-") {
+            if (
+              (lastBefore === "+") |
+              (lastBefore === "-") |
+              (lastBefore === "/") |
+              (lastBefore === "*")
+            ) {
+              state.array.splice(state.array.length - 2, 1);
+            }
+          }
+          state.array.push(element);
+          break;
+        case ".":
+          // checks if it is the first value
+          if (
+            (lastElement === "+") |
+            (lastElement === "-") |
+            (lastElement === "/") |
+            (lastElement === "*") |
+            (state.array.length === 0)
+          ) {
+            element = "0.";
+          } else {
+            // checks if it is second decimal
+            if (lastElement.includes(".")) break;
+            // combain it to make it a single string
+            element = state.array[state.array.length - 1].concat(element);
+            state.array.pop();
+          }
+          state.array.push(element);
+          break;
+        default:
+          //removes useless zeros
+          if (lastElement === "0") {
+            state.array.pop();
+            lastElement = state.array[state.array.length - 1];
+            lastBefore = state.array[state.array.length - 2];
+          }
+          //makes numbers as a single string
+          if (
+            !isNaN(lastElement) | (lastElement === "-" && isNaN(lastBefore))
+          ) {
+            element = state.array[state.array.length - 1].concat(element);
+            state.array.pop();
+          }
+          state.array.push(element);
+          break;
+      }
+    },
+    Clear: (state) => {
+      // clears all values from the state
+      state.array = [];
+      state.result = 0;
+    },
+    Equal: (state) => {
+      //removes useless operators in back
+      if (isNaN(state.array[state.array.length - 1])) state.array.pop();
+      //calculates value and set array to empty
+      let calculate = state.array.join("");
+      state.result = eval(calculate);
+      state.array = [];
+    },
+  },
+});
+
+export const { Element, Clear, Equal } = calculatorSlice.actions;
+
+export default calculatorSlice.reducer;
